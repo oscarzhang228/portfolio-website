9a26729f12a36167aa15f05aa068e0f64f7e9755
Oscar Zhang <oscarzhang228@gmail.com>
2023-03-23 23:32:22 -0400

    set up skeleton of starting page and taskbar at bottom

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+class Main extends Component {
+  render() {
+    return (
+      <div className="">
+        <div className="container-fluid bg-black">
+          <h1 className="text-start">Hello</h1>
+        </div>
+        <div className="fixed-bottom border container-fluid h-5 bg-light">
+          <h2>Hello</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
